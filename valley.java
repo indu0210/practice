7b5f23d4c0abde0db6555c8aa538497365b5a806
indu0210 <54941555+indu0210@users.noreply.github.com>
2021-07-15 22:52:45 +0530

    Add files via upload

---
+++ valley.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int countingValleys(int steps, String path) {
+    // Write your code here
+        int s = 0, c = 0, i = 0;
+        char[] paths = path.toCharArray();
+        for (i = 0; i < steps; i++){
+            if (paths[i] == 'U'){
+                if (++s == 0)
+                    c++;
+            }
+            else s--;
+        }
+        return c;
+    }
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
